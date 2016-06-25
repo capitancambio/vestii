b33bdc30b2d320074d785da91a56c198e6c61683
DavidT3 <dt237@sussex.ac.uk>
2016-06-25 13:09:26 +0100

    Altered Python code and working on Arduino

--- VestiiInterface.py
+++ VestiiInterface.py
@@ -1,10 +1,111 @@
 from __future__ import division
+import serial
+import glob
+import platform
+import os
+import numpy as np
+
+
+"""A part of the Vestii project during Hack the Senses
+Written by: David Turner <dt237@sussex.ac.uk>
+Last modified by: David Turner on 25 June, 2016
+Copyright (c) 2016 David J Turner.  All right reserved
+"""
 
 __author__ = 'David Turner'
-# A part of the Vestii project during Hack the Senses
-# Written by: David Turner <dt237@sussex.ac.uk>
-# Last modified by: David Turner on 22 April, 2016
-# Copyright (c) 2016 David J Turner.  All right reserved
+NumOfElectrodes = 2
+
+
+class Vestii(object):
+
+    def __init__(self):  # Runs on declaration of new Vestii object
+        SysID = platform.system()  # Uses platform module to identify Windows/Mac/Other
+
+        if SysID == 'Windows':
+            Ports = list(serial.tools.list_ports.comports())  # List of Windows Ports
+            for i in range(0, len(Ports)):
+                if any("Arduino" in s for s in Ports[i]):
+                    ArduinoPort = (str(Ports[i]))[2:6]  # Searches the ports for Vestii
+
+        elif SysID == 'Darwin':
+            PortString = str(glob.glob("/dev/tty.usbmodem*")) # OS X port list
+            ArduinoPort = PortString[2: (len(PortString)) - 2]
+
+        else:
+            ArduinoPort = "/dev/" + os.popen("dmesg | egrep ttyACM | cut -f3 -d: | tail -n1").read().strip()
+
+        try:  # Tries to connect to the Arduino, if not possible then error is displayed
+            self.Interface = serial.Serial(ArduinoPort, 9600)
+            self.Connected = True  # Interface and Connected defined as class attributes
+        except OSError:
+            print('There seems to be an issue connecting')
+            print('Either the Arduino is not connected, or Arduino serial monitor is open and accessing the port' + '\n')
+            self.Connected = False
+
+        self.History = {}
+
+    def UpdateVestii(self, EListPol, EListCur):
+        EDict = {}
+
+        for i in range(0, len(EListPol)):
+            EDict["ElectrodePol{0}".format(i)] = EListPol[i]
+            EDict["ElectrodeCur{0}".format(i)] = EListCur[i]
+
+        self.State = EDict
+
+        if self.State != self.History:
+            if self.Connected is True:  # Only send update if data has changed
+                self.Interface.write(str(ChangeMatrix[y, x]) + '#' + str((self.shape * y) + x + 1) + '\n')
+                # Utilises serial connection to Arduino
+
+            if self.Connected is False:
+                print('The fuck you doing')
+                print(self.state)
+
+        self.History = self.State
 
 print('------------------Vestii V1------------------')
-print('')
+print('Control Vestii project from a computer')
+
+EListPol = [0, 1, 0, 1]
+EListCur = [0.3, 0.2, 0.3, 0.1]
+
+Device = Vestii()
+Device.SetState(EListPol, EListCur)
+
+
+
+
+
+
+
+
+"""
+
+def setState(self, AngleState):  # To be called when the stored state of the display needs to be changed
+    ChangedTo = np.zeros((self.shape[0], self.shape[1]))  # AngleState MUST be passed in as a sLen by sLen matrix
+    for y in xrange(self.shape[0]):  # Moves through the 2D array representing the TGD
+        for x in xrange(self.shape[1]):
+            ChangedTo[y, x] = (-1 if self.state[y, x] == AngleState[y, x] else AngleState[y, x])
+            # ChangedTo matrix stores the new values of changed taxels, no change is represented by -1
+    self.state = AngleState  # Then updates current state
+    return ChangedTo
+
+
+def updateDisplay(self, ChangeMatrix):  # Pushes update to display
+    for y in xrange(self.shape[0]):  # Cycles through every taxel on display
+        for x in xrange(self.shape[1]):
+            if self.Connected is True and ChangeMatrix[y, x] > 0:  # Will only move taxels that have changed
+                self.Interface.write(str(ChangeMatrix[y, x]) + '#' + str((self.shape * y) + x + 1) + '\n')
+                # Utilises serial connection to Arduino
+    if self.Connected is False:
+        print(ChangeMatrix)
+        print(self.state)
+
+
+def resetDisplay(self):  # Sets all taxels to zero state
+    Reset = np.zeros((self.shape[0], self.shape[1]), dtype=int)
+    Reset.fill(MinServoAng)
+    ChangedTo = TGD.setState(self, Reset)
+    TGD.updateDisplay(self, ChangedTo)
+"""
--- arduino/controller/controller.ino
+++ arduino/controller/controller.ino
@@ -24,6 +24,8 @@ void setup() {
   analogWrite(pin_electrode_2_current,0);
   digitalWrite(pin_electrode_1_polarity,LOW);
   digitalWrite(pin_electrode_2_polarity,LOW);
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -32,7 +34,42 @@ void loop() {
   
   int electrode_1_current = 0;
   int electrode_2_current = 0;
-  
+
+
+  while (Serial.available() > 0)  // While there is something in the Serial buffer   // Maybe serial isn't available
+    {
+
+      char Incoming = Serial.read();
+      InBuffer += Incoming;  // Concatonates individual characters from buffer into meaningful string
+
+      if(Incoming == '\n') {
+        CharNum = InBuffer.indexOf('#');
+        Sub1 = InBuffer.substring(0, CharNum);
+        Sub2 = InBuffer.substring((CharNum + 1));
+        CharNum2 = Sub2.indexOf('\\');
+        Sub3 = Sub2.substring(0,CharNum2);
+        TaxelID = ((Sub3.toInt()) + 1);
+        Angle = Sub1.toInt();
+
+        ServoArray[TaxelID].attach(TaxelID);
+        ServoArray[TaxelID].write(Angle);
+
+
+        InBuffer = ""; // This resets the buffer
+          }}
+
+
+
+
+
+
+
+
+
+
+
+
+
   if (Serial.available() > 0) {
 
   }
