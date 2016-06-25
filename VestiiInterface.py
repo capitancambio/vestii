90868a9df139fb3fe8d03e1818d28fdc3a976663
DavidT3 <dt237@sussex.ac.uk>
2016-06-25 11:16:08 +0100

    Commit Test

---
+++ VestiiInterface.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+__author__ = 'David Turner'
+# A part of the Vestii project during Hack the Senses
+# Written by: David Turner <dt237@sussex.ac.uk>
+# Last modified by: David Turner on 22 April, 2016
+# Copyright (c) 2016 David J Turner.  All right reserved
+
+print('------------------Vestii V1------------------')
+print('')
